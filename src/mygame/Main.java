11764cff49e9d1f0d0568c184fb90dd176113f96
Joomlah <DShelley1992@gmail.com>
2014-02-05 15:34:09 -0500

    Models into their own folder and converted to j3o.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -2,6 +2,9 @@ package mygame;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Spatial;
@@ -10,7 +13,10 @@ import com.jme3.scene.Spatial;
  * test
  * @author normenhansen
  */
-public class Main extends SimpleApplication {
+public class Main extends SimpleApplication 
+{
+    
+    Spatial bishop, king, knight, pawn, queen, rook;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -21,25 +27,65 @@ public class Main extends SimpleApplication {
     public void simpleInitApp() 
     {
         flyCam.setMoveSpeed(25f);
-        Spatial golem = assetManager.loadModel("Models/watergolem/watergolem.mesh.j3o");
+        cam.setLocation(new Vector3f(0, 0, 100));
         
-        golem.scale(0.5f);
+        bishop = assetManager.loadModel("Models/bishop/bishop.mesh.j3o");
+        king = assetManager.loadModel("Models/king/king.mesh.j3o");
+        knight = assetManager.loadModel("Models/knight/knight.mesh.j3o");
+        pawn = assetManager.loadModel("Models/pawn/pawn.mesh.j3o");
+        queen = assetManager.loadModel("Models/queen/queen.mesh.j3o");
+        rook = assetManager.loadModel("Models/rook/rook.mesh.j3o");
+
+        cleanPiece(bishop);
+        cleanPiece(king);
+        cleanPiece(knight);
+        cleanPiece(pawn);
+        cleanPiece(queen);
+        cleanPiece(rook);
         
-        DirectionalLight dl = new DirectionalLight();
-        dl.setDirection(new Vector3f(0, 0, -2f));
+        pawn.setLocalTranslation(-25, 0, 0);
+        queen.setLocalTranslation(-50, 0, 0);
+        king.setLocalTranslation(25, 0, 0);
+        knight.setLocalTranslation(50, 0, 0);
+        rook.setLocalTranslation(75, 0, 0);
         
-        DirectionalLight dl2 = new DirectionalLight();
-        dl.setDirection(new Vector3f(5f, 0, -2f));
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(1,0,-2).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
         
-        golem.addLight(dl);
-        golem.addLight(dl2);
+        rootNode.addLight(sun);
+        rootNode.attachChild(bishop);
+        rootNode.attachChild(king);
+        rootNode.attachChild(pawn);
+        rootNode.attachChild(queen);
+        rootNode.attachChild(knight);
+        rootNode.attachChild(rook);
+    }
+    
+    
+    /**
+     * Used to rotate and scale the chess pieces.
+     * @param piece 
+     */
+    private void cleanPiece(Spatial piece)
+    {
+        piece.scale(0.25f);
         
-        rootNode.attachChild(golem);
+        Quaternion rot = new Quaternion();
+        rot.fromAngleAxis(FastMath.PI / 2, new Vector3f(1, 0, 0));
+        piece.setLocalRotation(rot);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
+        Vector3f v = new Vector3f(0, 0, 0);
+        bishop.rotate(0, 0, v.z + 2 * tpf);
+        king.rotate(0, 0, v.z + 2 * tpf);
+        knight.rotate(0, 0, v.z + 2 * tpf);
+        pawn.rotate(0, 0, v.z + 2 * tpf);
+        queen.rotate(0, 0, v.z + 2 * tpf);
+        rook.rotate(0, 0, v.z + 2 * tpf);
     }
 
     @Override
